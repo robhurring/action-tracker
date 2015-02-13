d240faeb10e8210570fc6d149fe729015b59d33c
robhurring <robhurring@gmail.com>
2015-02-13 17:57:59 -0500

    quotes

--- lib/tracker/railtie.rb
+++ lib/tracker/railtie.rb
@@ -1,6 +1,6 @@
 module Tracker
   class Railtie < Rails::Railtie
-    initializer "tracker.subscriber" do
+    initializer 'tracker.subscriber' do
       ActiveSupport::Notifications.subscribe(TRACKER_NOTIFICATION_NAME) do |*args|
         event = Tracker::Event.new(*args)
         Tracker.track(event)
